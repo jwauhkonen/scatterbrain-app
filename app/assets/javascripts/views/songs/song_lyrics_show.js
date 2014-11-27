38c434f37d0ac7e397da3466920aee611af0b26b
Jonathan Wauhkonen <jwauhkonen@gmail.com>
2014-11-26 17:09:32 -0800

    start on select functionality

--- app/assets/javascripts/views/songs/song_lyrics_show.js
+++ app/assets/javascripts/views/songs/song_lyrics_show.js
@@ -1,10 +1,19 @@
 Scatterbrain.Views.SongLyricsShow = Backbone.View.extend ({
 	template: JST["songs/lyrics_show"],
 	
+	events: {
+		"select p": "handleSelect"
+		// this works for click, how does select work?
+	},
+	
 	render: function () {
 		var renderedContent = this.template({song: this.model});
 		this.$el.html(renderedContent);
 		return this;
+	},
+	
+	handleSelect: function (event) {
+		console.log("you selected some text!")
 	}
 	
 });
