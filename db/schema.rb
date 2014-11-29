a51eae4c7732543c77448a5206db7d178b94ca7c
Jonathan Wauhkonen <jwauhkonen@gmail.com>
2014-11-29 12:27:33 -0800

    migration for tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129194516) do
+ActiveRecord::Schema.define(version: 20141129195456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,12 @@ ActiveRecord::Schema.define(version: 20141129194516) do
   add_index "taggings", ["segment_id"], name: "index_taggings_on_segment_id", using: :btree
   add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
   add_index "taggings", ["user_id"], name: "index_taggings_on_user_id", using: :btree
+
+  create_table "tags", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
