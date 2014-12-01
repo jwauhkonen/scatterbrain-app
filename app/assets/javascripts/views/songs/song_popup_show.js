1189bb1fb01bf2ba2477ebdf363d8700f2920e0c
Jonathan Wauhkonen <jwauhkonen@gmail.com>
2014-11-30 22:13:47 -0800

    added groovy rainbow effect

--- app/assets/javascripts/views/songs/song_popup_show.js
+++ app/assets/javascripts/views/songs/song_popup_show.js
@@ -5,6 +5,10 @@ Scatterbrain.Views.SongPopupShow = Backbone.View.extend ({
 		this.listenTo(this.collection, "sync", this.render);
 	},
 	
+	events: {
+		
+	},
+	
 	render: function () {
 		var renderedContent = this.template({song: this.model, tags: this.collection});
 		this.$el.html(renderedContent);
