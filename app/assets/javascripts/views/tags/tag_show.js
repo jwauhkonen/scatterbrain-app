378855448c6c86105c237f42e2394ed16d55afa4
Jonathan Wauhkonen <jwauhkonen@gmail.com>
2014-12-01 17:17:15 -0800

    trying to sort tag segments

--- app/assets/javascripts/views/tags/tag_show.js
+++ app/assets/javascripts/views/tags/tag_show.js
@@ -3,11 +3,27 @@ Scatterbrain.Views.TagShow = Backbone.View.extend ({
 	
 	initialize: function () {
 		this.listenTo(this.model, "sync", this.render);
+		this.sortTagSegments();
 	},
 	
 	render: function () {
 		var renderedContent = this.template({tag: this.model});
 		this.$el.html(renderedContent);
 		return this;
+	},
+	
+	sortTagSegments: function () {
+		var tag = this.model;
+		debugger
+		tag.segments().sort(function (a, b) {
+			if (a.taggingsByTagId(tag.id).length > b.taggingsByTagId(tag.id).length) {
+				return 1;
+			}
+			if (a.taggingsByTagId(tag.id).length < b.taggingsByTagId(tag.id).length) {
+				return -1;
+			}
+			return 0;
+		});
+		debugger
 	}
 });
