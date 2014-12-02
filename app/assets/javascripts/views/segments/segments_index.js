927fad4eeb4a3f77b115d2be129958ffac79d477
Jonathan Wauhkonen <jwauhkonen@gmail.com>
2014-12-01 21:55:13 -0800

    segment index shows top tags

--- app/assets/javascripts/views/segments/segments_index.js
+++ app/assets/javascripts/views/segments/segments_index.js
@@ -2,6 +2,7 @@ Scatterbrain.Views.SegmentsIndex = Backbone.View.extend ({
 	template: JST["segments/index"],
 	
 	initialize: function () {
+		this.listenTo(this.collection, "sync", this.sortSegmentTags);
 		this.listenTo(this.collection, "sync", this.render);
 	},
 	
@@ -10,6 +11,20 @@ Scatterbrain.Views.SegmentsIndex = Backbone.View.extend ({
 		this.$el.html(renderedContent);
 		return this;
 	},
+	
+	sortSegmentTags: function () {
+		this.collection.forEach( function (segment) {
+			segment.tags().models.sort(function (a, b) {
+				if (segment.taggingsByTagId(a.id).length > segment.taggingsByTagId(b.id).length) {
+					return -1;
+				}
+				if (segment.taggingsByTagId(a.id).length < segment.taggingsByTagId(b.id).length) {
+					return 1;
+				}
+				return 0;
+			});	
+		})
+	}
 	
 	// do sort tags here, similar to the other thing, then calculate the top tag in a similar way.
 	// I might need to also make a tagsByTaggingId or something.
