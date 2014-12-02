4b368162090192fd49d3b8ffcbe615bb5e5cade5
Jonathan Wauhkonen <jwauhkonen@gmail.com>
2014-12-02 11:18:55 -0800

    song index is sorted by tagging count

--- app/assets/javascripts/models/song.js
+++ app/assets/javascripts/models/song.js
@@ -9,11 +9,38 @@ Scatterbrain.Models.Song = Backbone.Model.extend ({
 		return this._segments;
 	},
 	
+	taggings: function () {
+		if (!this._taggings) {
+			this._taggings = new Scatterbrain.Collections.Taggings([], {song: this});
+		}
+		
+		return this._taggings;
+	},
+	
+	tags: function () {
+		if (!this._tags) {
+			this._tags = new Scatterbrain.Collections.Tags([], {song: this});
+		}
+		
+		return this._tags;
+	},
+	
 	parse: function (response) {
 		if (response.segments) {
 			this.segments().set(response.segments, {parse: true});
 			delete response.segments;
 		}
+		
+		if (response.taggings) {
+			this.taggings().set(response.taggings, {parse: true});
+			delete response.taggings;
+		}
+		
+		if (response.tags) {
+			this.tags().set(response.tags, {parse: true});
+			delete response.tags;
+		}
+		
 		return response;
 	}
 });
--- app/assets/javascripts/views/songs/songs_index.js
+++ app/assets/javascripts/views/songs/songs_index.js
@@ -2,6 +2,7 @@ Scatterbrain.Views.SongsIndex = Backbone.View.extend ({
 	template: JST["songs/index"],
 	
 	initialize: function () {
+		this.listenTo(this.collection, "sync", this.sortSongsByTaggingCount);
 		this.listenTo(this.collection, "sync", this.render);
 	},
 	
@@ -9,5 +10,17 @@ Scatterbrain.Views.SongsIndex = Backbone.View.extend ({
 		var renderedContent = this.template({songs: this.collection});
 		this.$el.html(renderedContent);
 		return this;
+	},
+	
+	sortSongsByTaggingCount: function () {
+		this.collection.models.sort( function(a, b) {
+			if (a.taggings().length > b.taggings().length) {
+				return -1;
+			}
+			if (a.taggings().length < b.taggings().length) {
+				return 1;
+			}
+			return 0;
+		})
 	}
 });
