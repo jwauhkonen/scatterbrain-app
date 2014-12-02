8e3412b45ffa88010ca3bc9d41d41d96434b29e4
Jonathan Wauhkonen <jwauhkonen@gmail.com>
2014-12-01 17:24:38 -0800

    sorted tag segments

--- app/assets/javascripts/views/tags/tag_show.js
+++ app/assets/javascripts/views/tags/tag_show.js
@@ -2,8 +2,8 @@ Scatterbrain.Views.TagShow = Backbone.View.extend ({
 	template: JST["tags/show"],
 	
 	initialize: function () {
+		this.listenTo(this.model, "sync", this.sortTagSegments);
 		this.listenTo(this.model, "sync", this.render);
-		this.sortTagSegments();
 	},
 	
 	render: function () {
@@ -14,16 +14,16 @@ Scatterbrain.Views.TagShow = Backbone.View.extend ({
 	
 	sortTagSegments: function () {
 		var tag = this.model;
-		debugger
-		tag.segments().sort(function (a, b) {
+		
+		tag.segments().models.sort(function (a, b) {
 			if (a.taggingsByTagId(tag.id).length > b.taggingsByTagId(tag.id).length) {
-				return 1;
+				return -1;
 			}
 			if (a.taggingsByTagId(tag.id).length < b.taggingsByTagId(tag.id).length) {
-				return -1;
+				return 1;
 			}
 			return 0;
 		});
-		debugger
+		
 	}
 });
