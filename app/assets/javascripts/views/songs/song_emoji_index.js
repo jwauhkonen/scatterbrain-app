9889def64793b6419ff2318935d8a185df292003
Jonathan Wauhkonen <jwauhkonen@gmail.com>
2014-12-04 14:01:10 -0800

    add emojis to song show page

--- app/assets/javascripts/views/songs/song_emoji_index.js
+++ app/assets/javascripts/views/songs/song_emoji_index.js
@@ -3,23 +3,12 @@ Scatterbrain.Views.SongEmojiIndex = Backbone.View.extend ({
 	
 	initialize: function () {
 		this.listenTo(this.collection, "sync", this.render);
+	},
+	
+	render: function () {
+		var renderedContent = this.template({tags: this.collection});
+		this.$el.html(renderedContent);
+		return this;
 	}
 	
 });
-
-
-
-// Scatterbrain.Views.SongPopupShow = Backbone.View.extend ({
-// 	template: JST["songs/popup_show"],
-//
-// 	initialize: function () {
-// 		this.listenTo(this.collection, "sync", this.render);
-// 	},
-//
-// 	render: function () {
-// 		var renderedContent = this.template({song: this.model, tags: this.collection});
-// 		this.$el.html(renderedContent);
-// 		return this;
-// 	}
-//
-// });
