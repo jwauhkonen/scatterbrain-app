a97ae970fbd54bc74417847d3a7f19b0b9a91501
Jonathan Wauhkonen <jwauhkonen@gmail.com>
2014-12-05 10:48:16 -0800

    song emojis show tag name on hover

--- app/assets/javascripts/views/songs/song_emoji_index.js
+++ app/assets/javascripts/views/songs/song_emoji_index.js
@@ -6,16 +6,43 @@ Scatterbrain.Views.SongEmojiIndex = Backbone.View.extend ({
 		this.listenTo(this.model, "sync", this.render);
 	},
 	
+	events: {
+		"mouseenter li.song-emoji": "revealEmojiName",
+		"mouseleave li.song-emoji": "hideEmojiName"
+	},
+	
 	render: function () {
-		var renderedContent = this.template({segmentId: null, song: this.model, tags: this.collection});
+		this.segmentId = null
+		var renderedContent = this.template({segmentId: this.segmentId, song: this.model, tags: this.collection});
 		this.$el.html(renderedContent);
 		return this;
 	},
 	
 	renderSegmentCounts: function (segmentId) {
-		var renderedContent = this.template({segmentId: segmentId, song: this.model, tags: this.collection});
+		this.segmentId = segmentId
+		var renderedContent = this.template({segmentId: this.segmentId, song: this.model, tags: this.collection});
 		this.$el.html(renderedContent);
 		return this;
+	},
+	
+	revealEmojiName: function (event) {
+		var emoji = $(event.currentTarget);
+		var emojiId = emoji.data('id');
+		var selector = '.song-emoji-count[data-id |= ' + emojiId + ']';
+		$(selector).html(this.collection.get(emojiId).get('name'));
+	},
+
+	hideEmojiName: function (event) {
+		var emoji = $(event.currentTarget);
+		var emojiId = emoji.data('id');
+		var song = this.model;
+		var tag = this.collection.get(emojiId);
+		var selector = '.song-emoji-count[data-id |= ' + emojiId + ']';
+		if (this.segmentId !== null) {
+			$(selector).html(new Scatterbrain.Collections.Taggings(song.taggingsBySegmentId(this.segmentId)).where({tag_id: tag.id}).length);
+		} else {
+			$(selector).html(song.taggingsByTagId(tag.id).length);
+		}
 	}
 	
 });
